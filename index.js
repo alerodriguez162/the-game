65f8a2082deb2ba6131dce5731e2ca9d633e0d07
andoni.resendiz <andoniresendiz@gmail.com>
2021-09-06 18:02:27 -0500

    funcion tarjetas

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const endScreenImg = document.querySelector(".end__screen--img");
 const canvas = document.getElementById("canvas");
 const infoList = document.querySelector(".info");
 const btnStartGame = document.getElementById("startGame");
+const ctx = canvas.getContext("2d");
 // ./images/bonk.webp
 const game = {
   setWelcomeView: function () {
@@ -87,20 +88,85 @@ class Board {
   checkIfLoose() {}
 }
 
+
 class Card {
   //TODO Seleccion de cartas
   constructor(type) {
     this.type = type;
     this.width = 50;
     this.height = 50;
-
-
+    this.img = new Image()
   }
-  function fruits() {
-    switch(this.type)
+  fruits() {
+    switch(this.type) {
+      case "apple":
+        this.img.src = '/images/cards/manzana.jpg'
+      break;
+      case "almond":
+        this.img.src = '/images/cards/almendra.jpg'
+      break;
+      case "coffee":
+        this.img.src = '/images/cards/cafe.jpg'
+      break;
+      case "donut":
+        this.img.src = '/images/cards/dona.jpg'
+      break;
+      case "cookie":
+        this.img.src = '/images/cards/galletas.jpg'
+      break;
+      case "burger":
+        this.img.src = '/images/cards/hamburguesa.jpg'
+      break;
+      case "ice":
+        this.img.src = '/images/cards/helado.jpg'
+      break;
+      case "jocho":
+        this.img.src = '/images/cards/hotdog.jpg'
+      break;
+      case "muffin":
+        this.img.src = '/images/cards/helado.jpg'
+      break;
+      case "ice":
+        this.img.src = '/images/cards/helado.jpg'
+      break;
+      case "pizza":
+        this.img.src = '/images/cards/pizza.jpg'
+      break;
+      case "cheese":
+        this.img.src = '/images/cards/ques.jpg'
+      break;
+      case "ramen":
+        this.img.src = '/images/cards/ramen.jpg'
+      break;
+      case "taco":
+        this.img.src = '/images/cards/taco.jpg'
+      break;
+      case "avocado":
+        this.img.src = '/images/aguacate.png'
+      break;
+      case "burrito":
+        this.img.src = '/images/burrito.jfif'
+      break;
+      case "fries":
+        this.img.src = '/images/fries.jfif'
+      break;
+      case "sushi":
+        this.img.src = '/images/sushi.jfif'
+      break;
+      case "potato":
+        this.img.src = '/images/potato.jpg'
+      break;
+    }
+
+    
+  }
+  drawCards(){
+  ctx.drawImage(this.img, 0, 0, this.width, this.height)
+    
   }
 
 }
+
 
 window.onload = () => {
   document.getElementById("home__btn").onclick = () => {
